1fcdc573528d51ed0ada812271405a11e0e035bc
Frederick Wagner <doctaphred@gmail.com>
2019-06-11 20:26:05 -0500

    Create attrs.py

---
+++ src/wabbit/utils/attrs.py
@@ -0,0 +1,37 @@
+class lazyattr:
+    """Non-data descriptor which replaces itself with an instance attribute.
+
+    >>> class LazyBoy:
+    ...    @lazyattr
+    ...    def ayy(self):
+    ...        print('ayy')
+    ...        return 'lmao'
+    >>> boy = LazyBoy()
+    >>> vars(boy)
+    {}
+    >>> boy.ayy
+    ayy
+    'lmao'
+    >>> vars(boy)
+    {'ayy': 'lmao'}
+    >>> boy.ayy
+    'lmao'
+    >>> del boy.ayy
+    >>> boy.ayy
+    ayy
+    'lmao'
+    """
+    def __init__(self, method, name=None):
+        self.method = method
+        self.name = name
+
+    def __set_name__(self, owner, name):
+        # Called when `self` is set on `owner`.
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        result = self.method(instance)
+        setattr(instance, self.name, result)
+        return result
