8ea0b7206a47e72d9155beb619b8e0bd3a6cf705
Frederick Wagner <doctaphred@gmail.com>
2019-06-12 15:54:51 -0500

    Update hellollvm.py

--- src/wabbit/hellollvm.py
+++ src/wabbit/hellollvm.py
@@ -1,5 +1,18 @@
 # hellollvm.py
-from llvmlite.ir import Module
+
+from llvmlite.ir import (
+    Constant,
+    Function,
+    FunctionType,
+    IntType,
+    IRBuilder,
+    Module,
+)
 
 mod = Module('hello')
+int_type = IntType(32)
+hello_func = Function(mod, FunctionType(int_type, []), name='hello')
+block = hello_func.append_basic_block('entry')
+builder = IRBuilder(block)
+builder.ret(Constant(IntType(32), 37))
 print(mod)
