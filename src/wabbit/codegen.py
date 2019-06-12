bc163c3d5d288cc7065b9b41f03c765e2713bb04
Frederick Wagner <doctaphred@gmail.com>
2019-06-12 15:45:48 -0500

    Add CTranspiler

--- src/wabbit/codegen.py
+++ src/wabbit/codegen.py
@@ -60,7 +60,7 @@ class Interpreter:
         print(self.pop())
 
 
-class Transpiler:
+class PythonTranspiler:
     def __init__(self):
         self.stack = []
         self.lines = []
@@ -103,11 +103,61 @@ class Transpiler:
         self.emit(f'    print({self.pop()})')
 
 
+class CTranspiler:
+    def __init__(self):
+        self.stack = []
+        self.lines = []
+
+    def translate(self, code):
+        self.emit('#include <stdio.h>')
+        self.emit('int main() {')
+        for op, *args in code:
+            getattr(self, f'translate_{op}')(*args)
+        self.emit('}')
+        return '\n'.join(self.lines)
+
+    def push(self, fragment):
+        self.stack.append(fragment)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def emit(self, line):
+        self.lines.append(line)
+
+    def translate_GLOBALI(self, name):
+        self.emit(f'    int {name};')
+
+    def translate_CONSTI(self, value):
+        self.push(repr(value))
+
+    def translate_STORE(self, name):
+        self.emit(f'    {name} = {self.pop()};')
+
+    def translate_LOAD(self, name):
+        self.push(name)
+
+    def translate_ADDI(self):
+        self.push(f'({self.pop()} + {self.pop()})')
+
+    def translate_MULI(self):
+        self.push(f'({self.pop()} * {self.pop()})')
+
+    def translate_PRINTI(self):
+        self.emit(fr'    printf("%i\n", {self.pop()});')
+
+
 if __name__ == '__main__':
     interp = Interpreter()
     interp.run(code)
 
-    transp = Transpiler()
-    python = transp.translate(code)
+    py_transp = PythonTranspiler()
+    python = py_transp.translate(code)
     print(python)
     exec(python)
+
+    c_transp = CTranspiler()
+    c = c_transp.translate(code)
+    print(c)
+    with open('ayy.c', 'w') as f:
+        print(c, file=f)
