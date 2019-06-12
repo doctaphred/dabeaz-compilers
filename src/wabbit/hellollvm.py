7b4757e4d8b3ec5f2643af7e3642ed2e91acb5e0
Frederick Wagner <doctaphred@gmail.com>
2019-06-12 16:32:11 -0500

    Update hellollvm.py

--- src/wabbit/hellollvm.py
+++ src/wabbit/hellollvm.py
@@ -19,3 +19,30 @@ print(mod)
 
 with open('hello.ll', 'w') as f:
     print(mod, file=f)
+
+
+def run_jit(module):
+    import llvmlite.binding as llvm
+
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()
+
+    target = llvm.Target.from_default_triple()
+    target_machine = target.create_target_machine()
+    compiled_mod = llvm.parse_assembly(str(module))
+    engine = llvm.create_mcjit_compiler(compiled_mod, target_machine)
+
+    # Look up the function pointer (a Python int)
+    func_ptr = engine.get_function_address("hello")
+
+    # Turn into a Python callable using ctypes
+    from ctypes import CFUNCTYPE, c_int
+    hello = CFUNCTYPE(c_int)(func_ptr)
+
+    res = hello()
+    print('hello() returned', res)
+
+
+# Run it!
+run_jit(mod)
