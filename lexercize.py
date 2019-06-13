3e6bdd560621db5062311ced32507ea35101b4ae
Frederick Wagner <doctaphred@gmail.com>
2019-06-13 14:21:20 -0500

    Create lexercize.py

---
+++ lexercize.py
@@ -0,0 +1,25 @@
+# lexercise.py
+
+text = "123 abc 45 + - * def 73"
+
+
+def find(text):
+    '''
+    Find all numbers and all words and prints them out.
+    Example:
+
+        NUMBER: 123
+        WORD: abc
+        NUMBER: 45
+        WORD: def
+        NUMBER: 73
+
+    Ignore all other characters.
+    '''
+    index = 0
+    while index < len(text):
+        ...
+        index += 1
+
+
+find(text)
